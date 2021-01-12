025e73d8fa22fed62a927cb64006a59e852c56c5
sealfury <willspecker@gmail.com>
2021-01-12 11:22:28 +0100

    Scaffolds react native application with basic functionality

--- App.jsx
+++ App.jsx
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useSelector } from 'react-redux'
+import ApplicationHeader from './components/ApplicationHeader'
 
-export default function App() {
+const App = () => {
+  const { appTitle } = useSelector(state => state)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ApplicationHeader />
+      <Text>{appTitle}</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +25,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
---
+++ state/store/initialState.js
@@ -0,0 +1,5 @@
+const initialState = {
+  appTitle: "Hello World Application"
+}
+
+export default initialState
